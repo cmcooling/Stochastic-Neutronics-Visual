149c905861873f0692c8e44185021aff762b2131
Chris Cooling <cmcooling@gmail.com>
2021-07-14 16:06:34 +0100

    Time Tracker

---
+++ Neutron Paths/Assets/TimeTracker.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeTracker : MonoBehaviour
+{
+    float simulatedTime;
+    public float timeDilation = 1;
+    public Text timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        simulatedTime = 0;
+        timeText = GameObject.Find("Canvas/Time Display").GetComponent("Text") as Text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        simulatedTime += Time.deltaTime * timeDilation;
+        timeText.text = simulatedTime.ToString() + "s";
+
+        Debug.Log(simulatedTime.ToString());
+    }
+}
