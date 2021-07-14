d2bcf5051ac5cf6d93c51104e40d89eedcd5ff98
Chris Cooling <cmcooling@gmail.com>
2021-07-14 16:22:35 +0100

    Fixed source spawner error

--- Neutron Paths/Assets/SourceSpawner.cs
+++ Neutron Paths/Assets/SourceSpawner.cs
@@ -16,18 +16,18 @@ public class SourceSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float simulatedInterval = 0.01f;
-        float rand = Random.Range(0.0f, 1.0f);
-        
+        float simulatedInterval = 0.01f;        
 
-        float currentTime = 0.0f;
-        while( currentTime < simulatedInterval)
+        float elapsedTime = 0.0f;
+        while( elapsedTime < simulatedInterval)
         {
+            float rand = Random.Range(0.0f, 1.0f);
             float timeToNext = -Mathf.Log(rand) / sourceStrength;
-            currentTime += timeToNext;
+            elapsedTime += timeToNext;
 
-            if (currentTime < simulatedInterval)
+            if (elapsedTime < simulatedInterval)
             {
+                Debug.Log(simulatedInterval + " "+ elapsedTime);
                 Vector3 startCoordinate = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f));
                 Instantiate(sourcePingPrefab, startCoordinate, Quaternion.identity);
             }
